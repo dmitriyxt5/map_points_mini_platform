edac8f37de2f6238817c9a9835f8aec5aeec9c7e
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-24 17:20:35 +0500

    static sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-set -e  # <-- exit on first error
+set -e
 
-python3 -m pip install --upgrade pip
+python3 -m venv venv
 
-python3 -m pip install -r requirements.txt
+source venv/bin/activate
 
-python3 manage.py collectstatic --noinput
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
