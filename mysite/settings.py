9b53b0c4e5235aa4c29a69d9f77d0677792995b7
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-24 16:18:41 +0500

    config correctly reading

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(DJANGO_ALLOWED_HOSTS)
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='*').split(',')
 # ['127.0.0.1', 'localhost']
 
 
