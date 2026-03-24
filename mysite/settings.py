8ab9717df1feac1c700fdb4556032d9dc42b8755
Dmitriy <dmitriy@Dmitriys-MacBook-Air.local>
2026-03-24 16:36:29 +0500

    files for deploy static

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' # Папка, куда WhiteNoise будет смотреть
+# STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static' # Папка, куда WhiteNoise будет смотреть
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
